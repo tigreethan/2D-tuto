fe6e1288abda17de7730c9bbd3efa1ba63b3aa4e
thesmartnath095 <54nathan.p@gmail.com>
2025-09-12 23:31:43 +0200

    Player Movement
    Déplacement fini

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,38 +15,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = 0.0f;
-        if (MoveAction.IsPressed())
-        {
-
-            horizontal = -0.01f;
-
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-
-            horizontal = + 0.01f;
-            
-        }
-        Debug.Log(horizontal);
-        float vertical = 0.0f;
-        if (Keyboard.current.downArrowKey.isPressed)
-        {
-
-            vertical = -0.01f;
-
-        }
-        else if (Keyboard.current.upArrowKey.isPressed)
-        {
-
-            vertical = + 0.01f;
-
-        }
-        Debug.Log(vertical);
-        Vector2 position = transform.position;
-        position.x = position.x + horizontal;
-        position.y = position.y + vertical;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.01f;
         transform.position = position;
-
+        
     }
 }
