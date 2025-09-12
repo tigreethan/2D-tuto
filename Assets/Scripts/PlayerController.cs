06450f4bed1a755cb3dda7121fcdc5410da92f02
Teami <102990328+tigreethan@users.noreply.github.com>
2025-09-12 23:09:29 +0200

    Player script + map tiles
    début mouvement joueur

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+     MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = 0.0f;
+        if (MoveAction.IsPressed())
+        {
+
+            horizontal = -0.01f;
+
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+
+            horizontal = + 0.01f;
+            
+        }
+        Debug.Log(horizontal);
+        float vertical = 0.0f;
+        if (Keyboard.current.downArrowKey.isPressed)
+        {
+
+            vertical = -0.01f;
+
+        }
+        else if (Keyboard.current.upArrowKey.isPressed)
+        {
+
+            vertical = + 0.01f;
+
+        }
+        Debug.Log(vertical);
+        Vector2 position = transform.position;
+        position.x = position.x + horizontal;
+        position.y = position.y + vertical;
+        transform.position = position;
+
     }
 }
